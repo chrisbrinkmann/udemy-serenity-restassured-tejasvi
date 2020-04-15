0fbfeef6890b6d900dc183da68d5210f396092a9
chrisbrinkmann <brinkmcd@gmail.com>
2020-04-15 15:34:47 -0400

    refactor student crud tests impl into serenity steps class

---
+++ src/test/java/com/udemy/tejasvi/studentapp/cucumber/serenity/StudentSerenitySteps.java
@@ -0,0 +1,72 @@
+package com.udemy.tejasvi.studentapp.cucumber.serenity;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.udemy.tejasvi.studentapp.model.StudentClass;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+
+public class StudentSerenitySteps {
+
+	@Step("Creating student with firstName: {0}, lastName:{1}, email:{2}, programme:{3}, courses:{4}")
+	public ValidatableResponse createStudent(String firstName, String lastName,
+			String email, String programme, List<String> courses) {
+
+		// initialize POJO for create request
+		StudentClass student = new StudentClass();
+		student.setCourses(courses);
+		student.setFirstName(firstName);
+		student.setLastName(lastName);
+		student.setEmail(email);
+		student.setProgramme(programme);
+
+		// POST /student create student & check status code
+		return SerenityRest.rest().given().contentType(ContentType.JSON).when()
+				.body(student).post().then(); // then() returns a
+												// ValidatableResponse
+	}
+
+	@Step("Getting the student object with firstName: {0}")
+	public HashMap<String, Object> getStudentByFirstName(String firstName) {
+		return SerenityRest.rest().given().when().get("/list").then().log()
+				.all().statusCode(200).extract()
+				.path("find{it.firstName=='" + firstName + "'}");
+	}
+
+	@Step("Updating student with studentID: {0} firstName: {1}, lastName:{2}, email:{3}, programme:{4}, courses:{5}")
+	public ValidatableResponse updateStudent(int id, String firstName,
+			String lastName, String email, String programme,
+			List<String> courses) {
+
+		// initialize POJO for update request
+		StudentClass student = new StudentClass();
+		student.setCourses(courses);
+		student.setFirstName(firstName);
+		student.setLastName(lastName);
+		student.setEmail(email);
+		student.setProgramme(programme);
+
+		// PUT /{id} update student
+		return SerenityRest.rest().given().log().all()
+				.contentType(ContentType.JSON).when().body(student)
+				.put("/" + id).then().log().all().statusCode(200);
+	}
+
+	@Step("Delete the student with the studentID:{0}")
+	public void deleteStudent(int id) {
+		// DELETE /{id} delete student
+		SerenityRest.rest().given().when()
+				.delete("/" + id);
+	}
+	
+	@Step("Get student with the ID{0}")
+	public ValidatableResponse getStudentById(int id) {
+		// GET /{id} assert 404 not found for deleted student
+			return	SerenityRest.rest().given().when()
+						.get("/" + id).then().log().all();
+	}
+}
--- src/test/java/com/udemy/tejasvi/studentapp/junit/studentsinfo/StudentsCRUDTest.java
+++ src/test/java/com/udemy/tejasvi/studentapp/junit/studentsinfo/StudentsCRUDTest.java
@@ -1,5 +1,8 @@
 package com.udemy.tejasvi.studentapp.junit.studentsinfo;
 
+import static org.hamcrest.Matchers.hasValue;
+import static org.junit.Assert.assertThat;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,153 +12,77 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
-import com.udemy.tejasvi.studentapp.model.StudentClass;
+import com.udemy.tejasvi.studentapp.cucumber.serenity.StudentSerenitySteps;
 import com.udemy.tejasvi.studentapp.testbase.TestBase;
 import com.udemy.tejasvi.studentapp.utils.Utils;
 
-import io.restassured.http.ContentType;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.Title;
-
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
 
 @RunWith(SerenityRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StudentsCRUDTest extends TestBase {
-	
+
+	@Steps
+	StudentSerenitySteps steps;
+
 	static String firstName = "testUser" + Utils.getRandomIntString();
 	static String lastName = "testUser" + Utils.getRandomIntString();
 	static String programme = "ComputerScience";
 	static String email = Utils.getRandomIntString() + "demouser@gmail.com";
+	static List<String> courses = new ArrayList<String>();
 	static HashMap<String, Object> studentMapCache;
-	
+
 	@Title("This test will create a new student")
 	@Test
 	public void aCreateStudent() {
-		List<String> courses = new ArrayList<String>();
 		courses.add("Java");
 		courses.add("C++");
-		
-		// initialize POJO for create request
-		StudentClass student = new StudentClass();
-		student.setCourses(courses);
-		student.setFirstName(firstName);
-		student.setLastName(lastName);
-		student.setEmail(email);
-		student.setProgramme(programme);
-		
-		// POST /student create student & check status code
-		SerenityRest
-			.rest()
-			.given()
-			.log()
-			.all()
-			.contentType(ContentType.JSON)
-			.when()
-			.body(student)
-			.post()
-			.then()
-			.log()
-			.all()
-			.statusCode(201);
+
+		steps.createStudent(firstName, lastName, email, programme, courses)
+				.statusCode(201);
 	}
-	
+
 	@Title("This test will verify a the student in the previous test was added")
 	@Test
 	public void bGetStudent() {
-		
 		// GET /list extract student & cache
-		studentMapCache = SerenityRest
-			.rest()
-			.given()
-			.when()
-			.get("/list")
-			.then()
-			.log()
-			.all()
-			.statusCode(200)
-			.extract()
-			.path("find{it.firstName=='"+ firstName+"'}");
-		
+		studentMapCache = steps.getStudentByFirstName(firstName);
+
 		System.out.println("The value is: " + studentMapCache);
-		
+
 		// assert student with firstName from previous step was returned
 		assertThat(studentMapCache, hasValue(firstName));
 	}
-	
+
 	@Title("Update user info and verify updated info")
 	@Test
 	public void cUpdateStudent() {
-		List<String> courses = new ArrayList<String>();
-		courses.add("Java");
-		courses.add("C++");
-		
-		// update firstName
+		// update firstName cache
 		firstName = firstName + "_updated";
-		
-		// initialize POJO for update request
-		StudentClass student = new StudentClass();
-		student.setCourses(courses);
-		student.setFirstName(firstName);
-		student.setLastName(lastName);
-		student.setEmail(email);
-		student.setProgramme(programme);	
-		
-		// PUT /{id} update student
-		SerenityRest
-			.rest()
-			.given()
-			.log()
-			.all()
-			.contentType(ContentType.JSON)
-			.when()
-			.body(student)
-			.put("/"+(int)studentMapCache.get("id"))
-			.then()
-			.log()
-			.all()
-			.statusCode(200);
-		
+
+		// PUT /{id} update student info by id
+		steps.updateStudent((int)studentMapCache.get("id"), firstName, lastName, email, programme, courses);
+
 		// GET /list extract student & cache
-		studentMapCache = SerenityRest
-				.rest()
-				.given()
-				.when()
-				.get("/list")
-				.then()
-				.log()
-				.all()
-				.statusCode(200)
-				.extract()
-				.path("find{it.firstName=='"+ firstName+"'}");
-		
+		studentMapCache = steps.getStudentByFirstName(firstName);
+
 		System.out.println("The value is: " + studentMapCache);
-		
+
 		// assert updated name was saved
 		assertThat(studentMapCache, hasValue(firstName));
 	}
-	
+
 	@Title("Delete a student and verify")
 	@Test
 	public void dDeleteStudent() {
 		// DELETE /{id} delete student
-		SerenityRest
-			.rest()
-			.given()
-			.when()
-			.delete("/" + (int)studentMapCache.get("id"));
-		
+		steps.deleteStudent((int)studentMapCache.get("id"));
+
 		// GET /{id} assert 404 not found for deleted student
-		SerenityRest
-			.rest()
-			.given()
-			.when()
-			.get("/" + (int)studentMapCache.get("id"))
-			.then()
-			.log()
-			.all()
+		steps.getStudentById((int)studentMapCache.get("id"))
 			.statusCode(404);
 	}
 }
