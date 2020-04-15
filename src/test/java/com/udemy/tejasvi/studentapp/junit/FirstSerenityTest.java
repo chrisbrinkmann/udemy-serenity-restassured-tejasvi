8b744efbf8c07881cdcdb70d197210021da93fc2
chrisbrinkmann <brinkmcd@gmail.com>
2020-04-14 20:48:09 -0400

    add tests to demo different serenity test outcomes in report

--- src/test/java/com/udemy/tejasvi/studentapp/junit/FirstSerenityTest.java
+++ src/test/java/com/udemy/tejasvi/studentapp/junit/FirstSerenityTest.java
@@ -1,12 +1,14 @@
 package com.udemy.tejasvi.studentapp.junit;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.restassured.RestAssured;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Pending;
 
 @RunWith(SerenityRunner.class)
 public class FirstSerenityTest {
@@ -27,5 +29,33 @@ public class FirstSerenityTest {
 				.log()
 				.all()
 				.statusCode(200);
+	}
+	
+	@Test
+	public void failTest() {
+		SerenityRest
+			.given()
+			.when()
+				.get("/student/list")
+			.then()
+				.log()
+				.all()
+				.statusCode(500);
+	}
+	
+	@Pending
+	@Test
+	public void pendingTest() {
+	}
+	
+	@Ignore
+	@Test
+	public void skippedTest() {
+	}
+	
+
+	@Test
+	public void errorTest() {
+		System.out.println("This is an error" + (5/0));
 	}
 }
